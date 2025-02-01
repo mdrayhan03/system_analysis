3ed368e72df2572402e4c2efa1afd2484dba8398
mdrayhan03 <phylosopherhossain2022@gmail.com>
2025-02-01 19:50:17 +0600

    commit

--- build_file.sh
+++ build_file.sh
@@ -1,4 +1,10 @@
+#!/bin/bash
 echo " BUILD START "
+python3.12 -m pip install --upgrade pip
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput --clear
+
+# Explicitly rename the static directory for Vercel
+mv staticfiles dist
+
 echo " BUILD END "
